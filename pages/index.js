b0c89db2ca4312873e49694544a4b98bec0ed593
fathonio <fathoniahmad1347@gmail.com>
2022-08-21 21:38:06 +0700

    handle state

--- pages/index.js
+++ pages/index.js
@@ -12,68 +12,93 @@ export default function Home() {
     setResult("");
   };
 
-  const backSpace = () => {};
+  const backSpace = () => {
+    setResult(result.slice(0, -1));
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Eror");
+    }
+  };
 
   return (
-    <div className={styles.container}>
-      <form>
-        <input className={styles.iput} type="text" value={result} />
-      </form>
-      <div className={styles.keypad}>
-        <button onClick={clear} id={styles.clear}>
-          Clear
-        </button>
-        <button onClick={handleClick} id={styles.backSpace}>
-          C
-        </button>
-        <button name="/" onClick={handleClick}>
-          &divide;
-        </button>
-        <button name="7" onClick={handleClick}>
-          7
-        </button>
-        <button name="8" onClick={handleClick}>
-          8
-        </button>
-        <button name="9" onClick={handleClick}>
-          9
-        </button>
-        <button name="*" onClick={handleClick}>
-          &times;
-        </button>
-        <button name="4" onClick={handleClick}>
-          4
-        </button>
-        <button name="5" onClick={handleClick}>
-          5
-        </button>
-        <button name="6" onClick={handleClick}>
-          6
-        </button>
-        <button name="-" onClick={handleClick}>
-          &ndash;
-        </button>
-        <button name="1" onClick={handleClick}>
-          1
-        </button>
-        <button name="2" onClick={handleClick}>
-          2
-        </button>
-        <button name="3" onClick={handleClick}>
-          3
-        </button>
-        <button name="+" onClick={handleClick}>
-          +
-        </button>
-        <button name="0" onClick={handleClick}>
-          0
-        </button>
-        <button name="." onClick={handleClick}>
-          .
-        </button>
-        <button onClick={handleClick} id={styles.result}>
-          =
-        </button>
+    <div>
+      <h1>CALCULATOR</h1>
+      <div className={styles.container}>
+        <form>
+          <input className={styles.iput} type="text" value={result} />
+        </form>
+        <div className={styles.keypad}>
+          <button
+            className={styles.highlight}
+            onClick={clear}
+            id={styles.clear}
+          >
+            Clear
+          </button>
+          <button
+            className={styles.highlight}
+            onClick={backSpace}
+            id={styles.backSpace}
+          >
+            Del
+          </button>
+          <button className={styles.highlight} name="/" onClick={handleClick}>
+            &divide;
+          </button>
+          <button name="7" onClick={handleClick}>
+            7
+          </button>
+          <button name="8" onClick={handleClick}>
+            8
+          </button>
+          <button name="9" onClick={handleClick}>
+            9
+          </button>
+          <button className={styles.highlight} name="*" onClick={handleClick}>
+            &times;
+          </button>
+          <button name="4" onClick={handleClick}>
+            4
+          </button>
+          <button name="5" onClick={handleClick}>
+            5
+          </button>
+          <button name="6" onClick={handleClick}>
+            6
+          </button>
+          <button className={styles.highlight} name="-" onClick={handleClick}>
+            &ndash;
+          </button>
+          <button name="1" onClick={handleClick}>
+            1
+          </button>
+          <button name="2" onClick={handleClick}>
+            2
+          </button>
+          <button name="3" onClick={handleClick}>
+            3
+          </button>
+          <button className={styles.highlight} name="+" onClick={handleClick}>
+            +
+          </button>
+          <button name="0" onClick={handleClick}>
+            0
+          </button>
+          <button name="." onClick={handleClick}>
+            .
+          </button>
+          <button
+            className={styles.highlight}
+            onClick={calculate}
+            id={styles.result}
+          >
+            =
+          </button>
+        </div>
       </div>
     </div>
   );
